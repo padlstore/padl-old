202e1e9d75343e4f33bdab332895fecf9e0df367
Nikhil Murthy <murthynikhil@live.com>
2018-06-08 21:57:01 -0400

    Fully add channel features (Firebase channel integration and segue to chats)

--- padl/MessageListViewController.swift
+++ padl/MessageListViewController.swift
@@ -21,25 +21,49 @@ class MessageListViewController: UITableViewController {
     var newChannelTextField: UITextField? // 2
     private var channels: [Channel] = [] // 3
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        channels.append(Channel(id: "1", name: "Channel1"))
-        channels.append(Channel(id: "2", name: "Channel2"))
-        channels.append(Channel(id: "3", name: "Channel3"))
-        self.tableView.reloadData()
+    private lazy var channelRef: DatabaseReference = Database.database().reference().child("channels")
+    private var channelRefHandle: DatabaseHandle?
+    
+    // MARK: Firebase related methods
+    private func observeChannels() {
+        // Use the observe method to listen for new
+        // channels being written to the Firebase DB
+        channelRefHandle = channelRef.observe(.childAdded, with: { (snapshot) -> Void in // 1
+            let channelData = snapshot.value as! Dictionary<String, AnyObject> // 2
+            let id = snapshot.key
+            if let name = channelData["name"] as! String!, name.characters.count > 0 { // 3
+                self.channels.append(Channel(id: id, name: name))
+                self.tableView.reloadData()
+            } else {
+                print("Error! Could not decode channel data")
+            }
+        })
     }
     
+    // MARK: View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        title = "RW RIC"
+        observeChannels()
     }
-
+    
+    deinit {
+        if let refHandle = channelRefHandle {
+            channelRef.removeObserver(withHandle: refHandle)
+        }
+    }
+    
+    
+    @IBAction func createChannel(_ sender: Any) {
+        if let name = newChannelTextField?.text { // 1
+            let newChannelRef = channelRef.childByAutoId() // 2
+            let channelItem = [ // 3
+                "name": name
+            ]
+            newChannelRef.setValue(channelItem) // 4
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -76,6 +100,14 @@ class MessageListViewController: UITableViewController {
         }
         
         return cell
+    }
+    
+    // MARK: UITableViewDelegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == Section.currentChannelsSection.rawValue {
+            let channel = channels[(indexPath as NSIndexPath).row]
+            self.performSegue(withIdentifier: "ShowChannel", sender: channel)
+        }
     }
     
     /*
